bffad68e9d793cb49d6145b139922b94dd4f6369
UniVerseUser <universe.app.sv@gmail.com>
2025-10-31 17:32:15 -0600

    Primer commit

---
+++ UniVerse/UniVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniVerseApp.swift
+//  UniVerse
+//
+//  Created by ESTUDIANTE on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct UniVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniVerseTests/UniVerseTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniVerseTests.swift
+//  UniVerseTests
+//
+//  Created by ESTUDIANTE on 31/10/25.
+//
+
+import Testing
+@testable import UniVerse
+
+struct UniVerseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
